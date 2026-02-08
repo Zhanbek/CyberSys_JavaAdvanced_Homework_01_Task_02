eca2c1fc0a7829091e6ec2a9abdaa325c9464053
Sarsenov <zhanbek.sarsenov@gmail.com>
2026-02-08 15:05:34 +0500

    Реалізує завдання

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+        list.add("F");
+        list.add("G");
+        list.add("H");
+        list.add("I");
+        list.add("J");
 
+        Iterator<String> iterator = list.iterator();
+        int count = 0;
+        while (iterator.hasNext()){
+            System.out.println(++count + ": " + iterator.next());
+        }
     }
 }
